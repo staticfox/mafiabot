8a6997b76908a3fb0c852464791a08091c12565e
nickthename <spamedkid@gmail.com>
2013-12-04 19:18:15 -0500

    Minor debugging in messages

--- messages.py
+++ messages.py
@@ -1,4 +1,11 @@
 def public(c, e):
+    print(e.arguments)
+    print(e.source.nick)
+    print(e.source)
+    print(e.target)
+    print(e)
+    print(dir(e))
+    print(e.__dict__)
     pass
 
 def private(c, e):
