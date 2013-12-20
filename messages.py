44e6f322e58b898f41f360ddaed1897384f8d7ef
nickthename <spamedkid@gmail.com>
2013-12-20 18:31:58 -0500

    I honestly don't know what I did, but somethingg

--- messages.py
+++ messages.py
@@ -16,23 +16,30 @@ def handler(c, e, bot):
     if e.source.nick in admins:
         if cmd == 'voicelist':
             print(bot.channels[bot.mchan].voiceddict)
+
         if cmd == 'voiceall':
             for user in bot.users:
                 bot.voice(user,True)
+
         if cmd == 'reset':
-            modes.purge_voice(bot)
+            modes.hardstop(bot)
+
         if cmd == 'talk':
             bot.say_main(" ".join(args))
+
         if cmd == 'quit': 
             c.disconnect('Quitted from IRC by %s' % e.source)
             sys.exit(0)
+
         elif cmd == 'disable':
             if not bot.state == 0:
                 c.privmsg(e.target, "Disabling.")
             bot.state = 0
+
         elif cmd == 'enable' and bot.state == 0:
             bot.state = 1
             c.privmsg(e.target, "Enabling.")
+
         if bot.state == 1:
             if cmd == 'start':
                 gamers = []
@@ -41,11 +48,13 @@ def handler(c, e, bot):
                         gamers.append(key)
                 print('Starting with {0}'.format(gamers))
                 modes.confirms(bot)
+
     if bot.state == 1:
         if cmd == 'join' and bot.users[e.source.nick] in [0,-1,-3]:
             bot.users[e.source.nick]=1
         if cmd == 'leave' and bot.users[e.source.nick] in [1,2]:
             bot.users[e.source.nick]=0
+
     elif bot.state == 2:
         if cmd == 'join' and bot.users[e.source.nick] in [0,-1,-3]:
             bot.users[e.source.nick]=2
@@ -53,3 +62,10 @@ def handler(c, e, bot):
             bot.users[e.source.nick]=2
         if cmd == 'leave' and bot.users[e.source.nick] in [1,2]:
             bot.users[e.source.nick]=0
+
+    elif bot.state == 5:
+        if 200<bot.users[e.source.nick]<251:
+            for user in bot.users:
+                if 200<bot.users[user]<251 and user!=e.source.nick:
+                    bot.say_main("{0}: {1}".format(e.source.nick,
+                                                cmd+"".join(args)))
--- modes.py
+++ modes.py
@@ -2,13 +2,26 @@ from getconfig import get_config_value
 import sys
 from defer import defer
 
-def purge_voice(bot):
+def hardstop(bot):
     admins = get_config_value('misc.admins').split('/')
     for user in bot.users:
         if user not in admins:
             bot.voice(user,False)
+        bot.users[user]=0
+    bot.state=0
+    bot.say_main("===========================")
+    bot.say_main("Game state completely reset")
+    bot.say_main("===========================")
+
 def confirms(bot):
+    gamers = []
+    for key in bot.users:
+        if bot.users[key]==1:
+            gamers.append(key)
     bot.state = 2
+    bot.say_main("Game is now in confirmation stage")
+    bot.say_main('Joined players: {0}'.format(",".join(gamers)))
+    bot.say_main('Joined players please message me "!confirm"')
     defer(int(get_config_value('game.confirmtimelimit'))-10,confirms_warn,bot)
     defer(int(get_config_value('game.confirmtimelimit')),wrap_start,bot)
 
@@ -25,4 +38,14 @@ def wrap_start(bot):
         if bot.users[user] == 2:
             bot.voice(user,True)
 
+def go_night(bot):
+    admins = get_config_value('misc.admins').split('/')
+    for user in bot.users:
+        if user not in admins and bot.users[user]>3:
+            bot.voice(user,False)           
 
+def go_day(bot):
+    admins = get_config_value('misc.admins').split('/')
+    for user in bot.users:
+        if user not in admins and bot.users[user]>3:
+            bot.voice(user,True)
